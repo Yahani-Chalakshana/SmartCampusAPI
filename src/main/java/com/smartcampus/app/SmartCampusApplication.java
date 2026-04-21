65d319ce735a56a4bea3bb026e78605e1f64df6b
Yahani Chalakshana <yahanichalakshana@gmail.com>
2026-04-21 10:39:36 +0530

    implement Request/Response Logging filter and finalize Application configuration

--- src/main/java/com/smartcampus/app/SmartCampusApplication.java
+++ src/main/java/com/smartcampus/app/SmartCampusApplication.java
@@ -8,6 +8,9 @@ import java.util.Set;
 
 import com.smartcampus.resource.DiscoveryResource;
 import com.smartcampus.resource.RoomResource;
+import com.smartcampus.resource.SensorResource;
+import com.smartcampus.exception.mapper.*;
+import com.smartcampus.filter.LoggingFilter;
 
 @ApplicationPath("/api/v1")
 public class SmartCampusApplication extends Application {
@@ -18,6 +21,14 @@ public class SmartCampusApplication extends Application {
         // Resources
         classes.add(DiscoveryResource.class);
         classes.add(RoomResource.class);
+        classes.add(SensorResource.class);
+        // Exception mappers
+        classes.add(RoomNotEmptyExceptionMapper.class);
+        classes.add(LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(SensorUnavailableExceptionMapper.class);
+        classes.add(GlobalExceptionMapper.class);
+        // Filter
+        classes.add(LoggingFilter.class);
         return classes;
     }
 }
